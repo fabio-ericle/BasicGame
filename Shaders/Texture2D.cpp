5d273be6f1d601e83b61250e267d4d24ed58725a
fabio-ericle <fabioericle@gmail.com>
2024-07-22 15:04:51 -0300

    Create Texture2D

---
+++ Shaders/Texture2D.cpp
@@ -0,0 +1,32 @@
+#include "Texture2D.h"
+
+Texture2D::Texture2D() : width(0), height(0), internalFornat(GL_RGB), imageFormat(GL_RGB), wrap_s(GL_REPEAT), wrap_t(GL_REPEAT), filterMin(GL_LINEAR), filterMax(GL_LINEAR)
+{
+	glGenTextures(1, &this->textureID);
+}
+
+Texture2D::~Texture2D()
+{
+	glDeleteTextures(1, &this->textureID);
+}
+
+void Texture2D::generate(GLuint width, GLuint height, unsigned char* data)
+{
+	this->width = width;
+	this->height = height;
+	
+	glBindTexture(GL_TEXTURE_2D, this->textureID);
+	glTexImage2D(GL_TEXTURE_2D, 0, this->internalFornat, this->width, this->height, 0, this->imageFormat, GL_UNSIGNED_INT, data);
+
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, this->wrap_s);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, this->wrap_t);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, this->filterMin);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, this->filterMax);
+
+	glBindTexture(GL_TEXTURE_2D, 0);
+}
+
+void Texture2D::bind() const
+{
+	glBindTexture(GL_TEXTURE_2D, this->textureID);
+}
---
+++ Shaders/Texture2D.h
@@ -0,0 +1,25 @@
+#pragma once
+#ifndef TEXTURE2D_H
+
+#include <iostream>
+#include <glad/gl.h>
+
+class Texture2D
+{
+public:
+	GLuint textureID;
+	GLuint width, height;
+	GLuint internalFornat, imageFormat;
+	GLuint wrap_s, wrap_t;
+	GLuint filterMin, filterMax;
+
+	Texture2D();
+	~Texture2D();
+
+	void generate(GLuint width, GLuint height, unsigned char* data);
+	void bind() const;
+};
+
+#endif // !TEXTURE2D_H
+
+
