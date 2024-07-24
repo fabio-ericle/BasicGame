b502943219a2830cd1d4fb056b2537f728a864d7
fabio-ericle <fabioericle@gmail.com>
2024-07-23 23:14:10 -0300

    Testing [SpriteRenderer]

--- Main.cpp
+++ Main.cpp
@@ -4,6 +4,11 @@
 
 #include "Shaders/Shader.h"
 #include "Utils/ResourceFile.h"
+#include "Shaders/SpriteRenderer.h"
+#include "Shaders/Texture2D.h"
+
+#define STB_IMAGE_IMPLEMENTATION
+#include "Utils/stb_image.h"
 
 constexpr const char* VERTEX_PATH = "Shaders/Sources/vertex.glsl";
 constexpr const char* FRAGMENT_PATH = "Shaders/Sources/fragment.glsl";
@@ -36,52 +41,55 @@ int main()
 
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
-    float vertices[] =
-    {
-        -0.5f,  0.5f,   // Top left
-        -0.5f, -0.5f,   // Bottom left
-         0.5f, -0.5f,   // Bottom right
-         0.5f,  0.5f    // Top right
-    };
-
-    int indices[] =
-    {
-        0, 1, 2,
-        0, 2, 3
-    };
-
     Shader* shader = new Shader(ResourceFile::FileToString(VERTEX_PATH), ResourceFile::FileToString(FRAGMENT_PATH));
 
-    //  VERTEX BUFFER OBJECT
-    GLuint VBO;
-    glGenBuffers(1, &VBO);
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    int width, height, channels;
+    unsigned char* data = stbi_load("container.jpg", &width, &height, &channels, 0);
 
-    //  VERTEX ARRAY OBJECT
-    GLuint VAO;
-    glGenVertexArrays(1, &VAO);
-    glBindVertexArray(VAO);
+    SpriteRenderer* render = new SpriteRenderer(*shader);
+    Texture2D* texture = new Texture2D();
 
-    GLuint position_location = glGetAttribLocation(shader->programID, "aPosition");
+    if (data)
+    {
+        texture->generate(width, height, data);
+    }
+    else
+    {
+        std::cout << "Failed to load texture" << std::endl;
+    }
+    stbi_image_free(data);
 
-    glVertexAttribPointer(position_location, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), (void*)0);
-    glEnableVertexAttribArray(position_location);
 
-    //  ELEMENT BUFFER OBJECT
-    GLuint EBO;
-    glGenBuffers(1, &EBO);
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+    glm::vec2 position = glm::vec2(1.0);
 
     while (!glfwWindowShouldClose(window))
     {
+        
         glClearColor(0.4f, 0.6f, 0.9f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
-        shader->use();
-        glBindVertexArray(VAO);
-        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+        {
+            position.x -= 0.2f;
+        }
+        if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+        {
+            position.x += 0.2f;
+        }
+        if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+        {
+            position.y -= 0.2f;
+        }
+        if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+        {
+            position.y += 0.2f;
+        }
+
+        glm::mat4 projection = glm::ortho(0.0f, static_cast<float>(800), static_cast<float>(600), 0.0f, -1.0f, 1.0f);
+        shader->setInt("fTexture", 0);
+        shader->setMat4("projection", projection);
+
+        render->drawSprite(*texture, glm::vec2(position), glm::vec2(100.f, 100.0f), 0.0f, glm::vec3(0.5f, 0.5f, 0.5f));
 
         glfwPollEvents();
         glfwSwapBuffers(window);
