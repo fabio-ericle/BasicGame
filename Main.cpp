738429028a2f7ec4a3a1f9a72fb5af039fde2569
fabio-ericle <fabioericle@gmail.com>
2024-07-24 23:07:04 -0300

    Update Main

--- Main.cpp
+++ Main.cpp
@@ -13,16 +13,23 @@
 constexpr const char* VERTEX_PATH = "Shaders/Sources/vertex.glsl";
 constexpr const char* FRAGMENT_PATH = "Shaders/Sources/fragment.glsl";
 
+constexpr int SCREEN_WIDTH = 800;
+constexpr int SCREEN_HEIGHT = 600;
+
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 
 int main()
 {
-    glfwInit();
+    if (!glfwInit())
+    {
+        std::cout << "\nERROR::GLFW::FAILED_TO_LOAD\n";
+    }
+
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-    GLFWwindow* window = glfwCreateWindow(800, 600, "game", NULL, NULL);
+    GLFWwindow* window = glfwCreateWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "game", NULL, NULL);
     if (!window)
     {
         std::cout << "\nERROR::GLFW::FAILED_TO_CREATE_A_WINDOW\n";
@@ -39,26 +46,26 @@ int main()
         return -1;
     }
 
+    glfwSwapInterval(1);
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
-    Shader* shader = new Shader(ResourceFile::FileToString(VERTEX_PATH), ResourceFile::FileToString(FRAGMENT_PATH));
+    Shader shader = Shader(ResourceFile::FileToString(VERTEX_PATH), ResourceFile::FileToString(FRAGMENT_PATH));
+    SpriteRenderer render = SpriteRenderer(shader);
+    Texture2D texture = Texture2D();
 
     int width, height, channels;
-    unsigned char* data = stbi_load("container.jpg", &width, &height, &channels, 0);
-
-    SpriteRenderer* render = new SpriteRenderer(*shader);
-    Texture2D* texture = new Texture2D();
+    unsigned char* data = stbi_load("./container.jpg", &width, &height, &channels, 0);
 
     if (data)
     {
-        texture->generate(width, height, data);
+        texture.generate(width, height, data);
     }
     else
     {
-        std::cout << "Failed to load texture" << std::endl;
+        std::cout << "\nERROR::STB_IMAGE::FALED_TO_LOAD_IMAGE\n";
     }
-    stbi_image_free(data);
 
+    stbi_image_free(data);
 
     glm::vec2 position = glm::vec2(1.0);
 
@@ -70,30 +77,72 @@ int main()
 
         if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
         {
-            position.x -= 0.2f;
+            position.x -= 1.2f;
         }
         if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
         {
-            position.x += 0.2f;
+            position.x += 1.2f;
         }
         if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
         {
-            position.y -= 0.2f;
+            position.y -= 1.2f;
         }
         if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
         {
-            position.y += 0.2f;
+            position.y += 1.2f;
         }
 
-        glm::mat4 projection = glm::ortho(0.0f, static_cast<float>(800), static_cast<float>(600), 0.0f, -1.0f, 1.0f);
-        shader->setInt("fTexture", 0);
-        shader->setMat4("projection", projection);
+        int screenWidth, screenHeight;
+        glfwGetWindowSize(window, &screenWidth, &screenHeight);
 
-        render->drawSprite(*texture, glm::vec2(position), glm::vec2(100.f, 100.0f), 0.0f, glm::vec3(0.5f, 0.5f, 0.5f));
+        // Terrain generator test
+        float r = 0.0f, g = 0.0f, b = 0.0f;
+        for (GLuint i = 0; i < 15; i++)
+        {
+            for (GLuint j = 0; j < 50; j++)
+            {
+                if (i == 6)
+                {
+                    r = 0.0f;
+                    g = 1.0f;
+                    b = 0.0f;
+                }
+                else if(i > 6 || i < 8)
+                {
+                    r = 1.0f;
+                    g = 0.7f;
+                    b = 0.5f;
+                }
+                else
+                {
+                    r = 0.8f;
+                    g = 0.8f;
+                    b = 0.8f;
+                }
+
+                glm::mat4 projection = glm::ortho(0.0f, static_cast<float>(screenWidth), static_cast<float>(screenHeight), 0.0f, -1.0f, 1.0f);
+                shader.setInt("fTexture", 0);
+                shader.setMat4("projection", projection);
+
+                if (i > 5)
+                {
+                    render.drawSprite(texture, glm::vec2(j * 50.0f, i * 50.0f), glm::vec2(50.f, 50.0f), 0.0f, glm::vec3(r, g, b));
+                }
+            }
+        }
+
+        glm::mat4 projection = glm::ortho(0.0f, static_cast<float>(screenWidth), static_cast<float>(screenHeight), 0.0f, -1.0f, 1.0f);
+        shader.setInt("fTexture", 0);
+        shader.setMat4("projection", projection);
+
+        render.drawSprite(texture, glm::vec2(position), glm::vec2(50.f, 50.0f), 0.0f, glm::vec3(1.0f, 0.5f, 0.5f));
 
         glfwPollEvents();
         glfwSwapBuffers(window);
     }
+
+    glfwTerminate();
+    return 0;
 }
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
